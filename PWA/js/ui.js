92b88adf8541a8d1c754381d450e0653f2aa9a20
CBreakr <rob.notwicz@gmail.com>
2019-10-11 20:08:00 -0400

    added event listeners for sidenavs

--- PWA/js/ui.js
+++ PWA/js/ui.js
@@ -1,2 +1,12 @@
 
 console.log("ui loaded");
+
+document.addEventListener("DOMContentLoaded", function() {
+    // nav menu
+    const menus = document.querySelectorAll(".side-menu");
+    M.Sidenav.init(menus, {edge: "right"});
+
+    // add recipe form
+    const forms = document.querySelectorAll(".side-form");
+    M.Sidenav.init(forms, {edge: "left"});
+});
