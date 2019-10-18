cfb2f6824ba750586f80757e3304912c84df67da
CBreakr <rob.notwicz@gmail.com>
2019-10-18 18:22:55 -0400

    added assets list to service worker for caching

--- sw.js
+++ sw.js
@@ -1,8 +1,30 @@
 
+const staticCacheName = "site-static";
+
+// the http requests
+const assets = [
+    "/",
+    "/index.html",
+    "/js/app.js",
+    "/js/ui.js",
+    "/js/materialize.js",
+    "/css/styles.css",
+    "/css/materialize.css",
+    "/images/dish.png",
+    "https://fonts.googleapis.com/icon?family=Material+Icons"
+];
+
 self.addEventListener("install", evt => {
     console.log("service worker has been installed");
     // we can add assets
     // useful for things which change rarely
+    caches.open(staticCacheName)
+    .then(cache => {
+        cache.addAll(assets);
+    })
+    .catch(err => {
+        console.log("error opening the cache", err);
+    });
 });
 
 self.addEventListener("activate", evt => {
